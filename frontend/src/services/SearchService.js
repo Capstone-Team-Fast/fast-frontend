0440758271160a91a4f2bb5a8327000c86649112
Liam <lbetterman@gmail.com>
2021-11-18 11:27:33 -0600

    added functionality to search by full name

--- frontend/src/services/SearchService.js
+++ frontend/src/services/SearchService.js
@@ -11,11 +11,13 @@ export default class SearchService{
                 let driverId = item.id;
                 let fName = item.first_name.toLowerCase();
                 let lName = item.last_name.toLowerCase();
+                let fullName = fName + " " + lName 
+                let formalName = lName + ", " + fName
                 let phone = item.phone;
                 let emp_status = item.employee_status.toLowerCase();
                 const filter = event.target.value.toLowerCase();
-                return driverId.toString().includes(filter) | fName.includes(filter) 
-                        | lName.includes(filter) | phone.toString().includes(filter) 
+                return driverId.toString().includes(filter) | fullName.includes(filter) 
+                        | formalName.includes(filter) | phone.toString().includes(filter) 
                         | emp_status.includes(filter);
             });
         }
@@ -34,13 +36,15 @@ export default class SearchService{
                 let recipientId = item.id.toString();
                 let fName = item.first_name.toLowerCase();
                 let lName = item.last_name.toLowerCase();
+                let fullName = fName + " " + lName 
+                let formalName = lName + ", " + fName
                 let phone = item.phone.toString();
                 let address = item.location.address.toLowerCase()
                 let city = item.location.city.toLowerCase() 
                 let zip = item.location.zipcode.toString()
                 const filter = event.target.value.toLowerCase();
-                return recipientId.includes(filter) | fName.includes(filter) 
-                        | lName.includes(filter) | phone.includes(filter) 
+                return recipientId.includes(filter) | fullName.includes(filter) 
+                        | formalName.includes(filter) | phone.includes(filter) 
                         | address.includes(filter) 
                         | city.includes(filter) | zip.includes(filter);
             });
