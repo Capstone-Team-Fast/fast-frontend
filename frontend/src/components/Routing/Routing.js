eece382a90d4c881ce43206cdaddc693b217d93f
kmpullen19 <kmpullen@unomaha.edu>
2021-12-05 11:21:04 -0600

    working on connecting routing

--- frontend/src/components/Routing/Routing.js
+++ frontend/src/components/Routing/Routing.js
@@ -26,50 +26,148 @@ constructor(props) {
      super(props);
      this.state  = {
          locations: [],
-         drivers:[],
-         recipients:[],
+         route: {
+          driver_ids:[],
+          client_ids:[],
+          delivery_limit: '',
+          departure: {
+            location: {},
+          }
+          // duration_limit: '',
+         }
      };
 
-     this.getCenter = this.getCenter.bind(this);
-     this.handleDriverCallback = this.handleDriverCallback.bind(this)
+    //  this.getCenter = this.getCenter.bind(this);
+     this.handleDriverCallback = this.handleDriverCallback.bind(this);
+     this.handleChange = this.handleChange.bind(this);
+     this.handleDuration = this.handleDuration.bind(this);
+     this.handleDeparture = this.handleDeparture.bind(this);
+     this.handleDeliveryLimit = this.handleDeliveryLimit.bind(this);
+     this.getEventValues = this.getEventValues.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
 }
 
-//     this.handleDriverDelete  =  this.handleDriverDelete.bind(this);
-// }
-
 componentDidMount() {
-     var  self  =  this;
-     locationService.getLocations().then(function (result) {
-         self.setState({ locations:  result});
-     });
+  var  self  =  this;
+  locationService.getLocations().then(function (result) {
+      self.setState({ locations:  result});
+  });
 }
 
 handleDriverCallback = (id, deselect) =>{
   if (deselect){
-    this.setState({drivers: this.state.drivers.filter(function(d) { 
-      return d !== id; 
-    })});
-    console.log("55", this.state.drivers);
+    this.setState(prevState => ({
+          route: {               // object that we want to update
+          ...prevState.route,    // keep all other key-value pairs
+          driver_ids : this.state.route.driver_ids.filter(function(d) { 
+            return d !== id; 
+          })}}));
   }
   else{
-    const newDrivers = this.state.drivers.concat({ id });
-    this.setState({drivers : newDrivers});
-    console.log("60", this.state.drivers);
+    if (this.state.route.driver_ids != null)
+    {
+      const newDrivers = this.state.route.driver_ids.concat({ id });
+      this.setState(prevState => ({
+        route : {
+        ...prevState.route,
+        driver_ids: newDrivers}
+      }));
+    }
+    else{
+      this.setState(prevState => ({
+        route : {
+        ...prevState.route,
+        driver_ids: id}
+      }));
+    }
+    
   }
 }
 
 handleRecipientCallback = (id, deselect) =>{
   if (deselect){
-    this.setState({recipients: this.state.recipients.filter(function(r) { 
-      return r !== id; 
-    })});
-    console.log("69", this.state.drivers);
+    this.setState(prevState => ({
+      route: {               // object that we want to update
+      ...prevState.route,    // keep all other key-value pairs
+      client_ids : this.state.route.client_ids.filter(function(r) { 
+        return r !== id; 
+      })}}));
   }
   else{
-    const newRecipients = this.state.recipients.concat({ id });
-    this.setState({recipients : newRecipients});
-    console.log("74", this.state.recipients);
+    if(this.state.route.client_ids != null){
+        const newRecipients = this.state.route.client_ids.concat({ id });
+        this.setState(prevState => ({
+          route : {
+          ...prevState.route,
+          client_ids: newRecipients}
+        }));
+    }
+    else{
+      this.setState(prevState => ({
+        route : {
+        ...prevState.route,
+        client_ids: id}
+      }));
+    }
+    
   }
+}
+
+/**
+ * Generic event handler that is called to update the component's state 
+ * when the user changes the value of a form field that does not require 
+ * special handling.
+ * @param {Object} event The event that is triggered on a change of value
+ *                          to a generic form field.
+ */
+ handleChange(event) {
+  let [value, name] = this.getEventValues(event);
+  this.setState(prevState => ({
+    route : {
+    ...prevState.route,
+    name: value}
+  }));
+}
+
+handleDeliveryLimit(event){
+  let [value, name] = this.getEventValues(event);
+  this.setState(prevState => ({
+    route : {
+    ...prevState.route,
+    delivery_limit: value}
+  }));
+}
+
+handleDuration(event){
+  let [value, name] = this.getEventValues(event);
+  this.setState(prevState => ({
+    route : {
+    ...prevState.route,
+    duration_limit: value}
+  }));
+}
+
+handleDeparture(event){
+  let [value, name] = this.getEventValues(event);
+  this.setState(prevState => ({
+    route : {
+    ...prevState.route,
+    departure: {
+      ...prevState.route.departure,
+      location: value
+    }
+    }}));
+}
+
+/**
+ * This method retrieves the value, name, and id properties of the 
+ * event that has been triggered.
+ * @param {Object} event The event that has been triggered.
+ * @returns The value, name, and id properties of the triggered event.
+ */
+ getEventValues(event) {
+  let [value, name] = [event.target.value, event.target.name];
+  return [value, name];
 }
 
 getCenter(location) {
@@ -80,11 +178,11 @@ getCenter(location) {
 
 handleSubmit = (event) => {
   event.preventDefault();
-  routeService.createRoute(this.state);
+  console.log(this.state.route);
+  routeService.createRoute(this.state.route);
   
   //#TODO: how to redirect to view route??
   //this.setState({redirect: "/"});
-  console.log(this.state)
 }
 
 render() {
@@ -96,20 +194,21 @@ render() {
         <Form.Group as={Col} controlId="formGridDeliveryLimit">
           <Form.Label className="title">Delivery Limit</Form.Label>
           <Form.Control type="number" placeholder="Driver Delivery Limit"
-                        onChange={this.handleChange} name="delivery_limit" min="1"/>
+                        onChange={this.handleDeliveryLimit} name="delivery_limit" min="1"/>
         </Form.Group>
         <Form.Group as={Col} controlId="formGridDurationLimit">
           <Form.Label className="title">Duration</Form.Label>
           <Form.Control type="number" placeholder="Duration Limit in Hours"
-                        onChange={this.handleChange} name="duration_limit" min="1"/>
+                        onChange={this.handleDuration} name="duration_limit" min="1"/>
         </Form.Group>
         <Form.Group as={Col} controlId="formGridDeparture">
           <Form.Label className="title">Departure Location</Form.Label>
-          <Form.Select onChange={this.handleChange} name="departure_location">
-            { this.state.locations.map( l => 
+          <Form.Select onChange={this.handleDeparture} name="departure_location">
+          { this.state.locations.map( l => 
                 <option>{this.getCenter(l)}</option>
             )}
-            </Form.Select>
+
+          </Form.Select>
         </Form.Group>
         </Row>   
           
