ab15c3fdaa55a37d6593e683c267407cac62cbde
isossa <sossajeovanis@gmail.com>
2021-12-30 20:48:14 -0600

    Added dialog box to entries deletion.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,19 +23,17 @@ import './App.css';
 
 const BaseLayout = () => (
   <div className="container-fluid">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">ISC Driver and Recipient Data</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link" href="/">ADD DATA</a>
-      <a className="nav-item nav-link" href="/routing">CREATE ROUTE</a>
-
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">HOME</a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="/">DATA</a>
+            <a className="nav-item nav-link" href="/routing">CREATE ROUTE</a>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
 
     <div className="content">
       <Route path="/" exact component={Driver} />
